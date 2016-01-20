331babe0391415bb318181b30c79b03cdfbd215e
Jiwoopark <shapley_aldebaran@kaist.ac.kr>
2016-01-20 21:09:21 +0900

    data binding and seperate cat constructor

--- js/app.js
+++ js/app.js
@@ -1,16 +1,34 @@
 var ViewModel = function(){
+    // this가 헷갈리니까 바인딩이 currentCat으로 들어갔는데 
+    // self로 this를 대체
+    var self = this;
+    this.currentCat = ko.observable ( new Cat() );
+    this.incrementCounter = function(){
+        self.currentCat().clickCount(self.currentCat().clickCount() + 1 );
+        
+    };
+}
+
+var Cat = function(){
     this.clickCount = ko.observable(0);
     this.name = ko.observable('Tabby');
-    this.level = ko.observable('Infant');
+    this.title = ko.observable('Infant');
     this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
     this.imgAttribution = ko.observable('');
 
-    this.incrementCounter = function(){
-        this.clickCount(this.clickCount() + 1 );
-        if(this.clickCount() > 10){
-            this.level('Big Baby');
+    this.nickname = ko.observableArray(['jiwoo','jang','sung','vong','ahn'])
+
+    this.title = ko.computed(function(){
+        var title;
+        var clicks = this.clickCount();
+        if( clicks > 5){
+            title = 'Newborn'
         }
-    };
+        else{
+            title = 'tabby';
+        }
+        return title;
+    }, this);
 }
 
 ko.applyBindings(new ViewModel());
