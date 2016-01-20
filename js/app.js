f19f5137ddd6f376a958d41ff4542a5361e8bf9d
Jiwoopark <shapley_aldebaran@kaist.ac.kr>
2016-01-20 21:58:20 +0900

    knockout catclikr

--- js/app.js
+++ js/app.js
@@ -1,22 +1,60 @@
+var initialCats = [
+    {
+        clickCount : 0,
+        name : 'Tabby',
+        imgSrc : 'img/22252709_010df3379e_z.jpg',
+        nicknames : ['jiwoo','jang','sung','vong','ahn']
+    },
+    {
+        clickCount : 0,
+        name : 'CSAPP',
+        imgSrc : 'img/434164568_fea0ad4013_z.jpg',
+        nicknames : ['gom']
+    },
+    {
+        clickCount : 0,
+        name : 'hw3',
+        imgSrc : 'img/1413379559_412a540d29_z.jpg',
+        nicknames : ['KaKao']
+    },
+    {
+        clickCount : 0,
+        name : 'office',
+        imgSrc : 'img/4154543904_6e2428c421_z.jpg',
+        nicknames : ['torrent']
+    }
+]
+
 var ViewModel = function(){
     // this가 헷갈리니까 바인딩이 currentCat으로 들어갔는데 
     // self로 this를 대체
     var self = this;
-    this.currentCat = ko.observable ( new Cat() );
+
+    this.catList = ko.observableArray([]);
+
+    initialCats.forEach(function(catItem){
+        self.catList.push( new Cat( catItem ));
+    })
+
+    self.currentCat = ko.observable( this.catList()[0]);
+
     this.incrementCounter = function(){
         self.currentCat().clickCount(self.currentCat().clickCount() + 1 );
         
     };
+
+    this.setCurrentCat = function(cat){
+        self.currentCat(cat);
+    }
 }
 
-var Cat = function(){
-    this.clickCount = ko.observable(0);
-    this.name = ko.observable('Tabby');
-    this.title = ko.observable('Infant');
-    this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
-    this.imgAttribution = ko.observable('');
+var Cat = function(data){
+    this.clickCount = ko.observable(data.clickCount);
+    this.name = ko.observable(data.name);
+    this.imgSrc = ko.observable(data.imgSrc);
+    this.imgAttribution = ko.observable(data.imgAttribution);
 
-    this.nickname = ko.observableArray(['jiwoo','jang','sung','vong','ahn'])
+    this.nickname = ko.observableArray(data.nicknames);
 
     this.title = ko.computed(function(){
         var title;
@@ -25,7 +63,7 @@ var Cat = function(){
             title = 'Newborn'
         }
         else{
-            title = 'tabby';
+            title = 'baby';
         }
         return title;
     }, this);
